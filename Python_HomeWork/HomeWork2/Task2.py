56a10a8e5b46ee8a7cef5f58292f9fecc4219b76
Mihael Bun <bunmihail@gmail.com>
2022-10-11 16:35:25 +0300

    Task 2.1 is complete

--- Python_HomeWork/HomeWork2/Task2.py
+++ Python_HomeWork/HomeWork2/Task2.py
@@ -1,26 +1,13 @@
-# Напишите программу, которая найдёт произведение пар чисел списка.
-# Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+# Напишите программу, которая принимает на вход число N и
+# выдает набор произведений чисел от 1 до N.
 # Пример:
-# - [2, 3, 4, 5, 6] => [12, 15, 16];
-# - [2, 3, 5, 6] => [12, 15]
+# - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
 
-from random import random, randint, randrange
-
-def generator(some_num):
-    x = []
-    for i in range(0, some_num):
-        x.append(randint(1, 9))
-    return x
+from re import I
 
 
-def pairsOfNums(nums, amount):
-    x = []
-    for i in range(0,  int(amount/2)):
-        x.append(nums[i] * nums[amount - 1 - i])
-    return x
-
-
-num_of_numbers = int(input('Please, write how many numbers do you want to write:'))
-result = generator(num_of_numbers)
-print(result)
-print(pairsOfNums(result, num_of_numbers))
+N = int(input('Write a number N for to find the product of numbers from 1 to N: '))
+product_of_numbers = 1
+for i in range(1, N + 1):
+    product_of_numbers *= i
+    print(f'{product_of_numbers}', end=" ")
