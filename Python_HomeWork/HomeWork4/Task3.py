2edd8a058ab4c4c7dae229577e2c9f120b34e4ae
Mihael Bun <bunmihail@gmail.com>
2022-10-28 04:40:20 +0300

    Task 3 update

--- Python_HomeWork/HomeWork4/Task3.py
+++ Python_HomeWork/HomeWork4/Task3.py
@@ -18,3 +18,25 @@ nums3 = [1, 1, 1, 5, 5, 6, 6, 7, 7, 3, 3, 2, 2]
 uniq_num(nums)
 uniq_num(nums2)
 uniq_num(nums3)
+
+
+# from random import randint as rI
+
+# unique = {}
+
+# my_list = ''.join(list(map(str, [rI(0,9) for i in range(20)])))
+# print(my_list)
+
+# for c in my_list:
+#     if unique.get(c):
+#         unique[c] = unique.get(c) + 1
+#     else:
+#         unique[c] = 1
+
+# u_list = []
+
+# for i in unique.items():
+#     if i[1] == 1:
+#         u_list.append(i[0])
+
+# print(*u_list)
