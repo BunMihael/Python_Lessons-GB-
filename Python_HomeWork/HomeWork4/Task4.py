c55a19c454edb2e437a43e36745f5098db3a6170
Mihael Bun <bunmihail@gmail.com>
2022-10-28 05:29:14 +0300

    Task 1 complete

--- Python_HomeWork/HomeWork4/Task4.py
+++ Python_HomeWork/HomeWork4/Task4.py
@@ -4,3 +4,15 @@
 # Коэффициенты должны быть случайными числами в диапазоне от 1 до 100
 # Пример:
 # - k=2 => 6*x^2 + 4*x + 5 = 0 или x^2 + 5 = 0 или 10*x^2 = 0
+
+from random import randint
+
+
+def coefficients_of_polynomial():
+    coef = {}
+    degree = int(input('Please enter the maximum degree: '))
+    for i in range(degree, -1, -1):
+        coef[i] = randint(1,100)
+    print(coef)
+
+coefficients_of_polynomial()
